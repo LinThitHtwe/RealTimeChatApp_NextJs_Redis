a72391e8a8132660931210f9007ef2870a03ae4d
LinThit27 <linthit2745@gmail.com>
2024-03-01 14:40:05 +0630

    [add] friend add function

---
+++ src/app/api/friends/add/route.ts
@@ -0,0 +1,58 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email: emailToAdd } = addFriendValidator.parse(body.email);
+    const idToAdd = (await fetchRedis(
+      "get",
+      `user:email:${emailToAdd}`
+    )) as string;
+
+    if (!idToAdd) {
+      return new Response("This person doesnt exist", { status: 400 });
+    }
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new Response(`Unauthorized`, { status: 401 });
+    }
+
+    if (idToAdd == session.user.id) {
+      return new Response("You cannot add yourself", { status: 400 });
+    }
+
+    const isAlreadyAdded = (await fetchRedis(
+      "sismember",
+      `user:${idToAdd}:incoming_friend_requests`,
+      session.user.id
+    )) as 0 | 1;
+
+    if (isAlreadyAdded) {
+      return new Response("ALready Added this User", { status: 400 });
+    }
+
+    const isAlreadyFriend = (await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    )) as 0 | 1;
+
+    if (isAlreadyFriend) {
+      return new Response("ALready Friend with this User", { status: 400 });
+    }
+
+    db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
+
+    return new Response("OK");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid Request Payload", { status: 422 });
+    }
+    return new Response("Invalid Request", { status: 400 });
+  }
+}
---
+++ src/helpers/redis.ts
@@ -0,0 +1,23 @@
+const upstashRedisRestURL = process.env.UPSTASH_REDIS_REST_URL;
+const authToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+type Command = "zrange" | "sismember" | "get" | "smembers";
+
+export async function fetchRedis(
+  command: Command,
+  ...args: (string | number)[]
+) {
+  const commandUrl = `${upstashRedisRestURL}/${command}/${args.join("/")}`;
+  const response = await fetch(commandUrl, {
+    headers: {
+      Authorization: `Baarer ${authToken}`,
+    },
+    cache: "no-store",
+  });
+
+  if (response.ok) {
+    throw new Error(`Error executing Redis command : ${response.statusText}`);
+  }
+  const data = await response.json();
+  return data.result;
+}
